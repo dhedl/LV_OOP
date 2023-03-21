8d9e1231381c00a0298d5902402af79b0059bcec
Daniel - Antonio Hedl (dhedl) <dhedl@tvz.hr>
2023-03-21 19:04:47 +0100

    2. laboratorijska vježba v2

--- LV2/lv2.py
+++ LV2/lv2.py
@@ -2,26 +2,26 @@ from datetime import date
 
 kolegij = {}
 
-kolegij['Ime kolegija'] = input('Unesite ime kolegija: ').upper()
-kolegij['Broj ECTS'] = int(input('Unesite ECTS bodove za kolegij: '))
+kolegij['Ime_kolegija'] = input('Unesite ime kolegija: ').upper()
+kolegij['Broj_ECTS'] = int(input('Unesite ECTS bodove za kolegij: '))
 
-# print('Kolegij', kolegij['Ime kolegija'], 'nosi', kolegij['Broj ECTS'], 'ECTS bodova')
+# print('Kolegij', kolegij['Ime_kolegija'], 'nosi', kolegij['Broj_ECTS'], 'ECTS bodova')
 
 ispit = {}
 
-ispit = kolegij.copy()
+ispit['Kolegij'] = kolegij
 dan = int(input('Unesite dan ispita: '))
 mjesec = int(input('Unesite mjesec ispita: '))
 godina = int(input('Unesite godinu ispita: '))
-ispit['Datum ispita'] = date(godina, mjesec, dan)
-# print('Kolegij', ispit['Ime kolegija'], 'nosi', ispit['Broj ECTS'], 'ECTS bodova')
-# print('Datum ispita je:', ispit['Datum ispita'])
+ispit['Datum_ispita'] = date(godina, mjesec, dan)
+# print('Kolegij', ispit['Kolegij']['Ime_kolegija'], 'nosi', ispit['Kolegij']['Broj_ECTS'], 'ECTS bodova')
+# print('Datum ispita je:', ispit['Datum_ispita'])
 
 student = {}
 
-student = ispit.copy()
-ime = input('Unesite ime studenta: ').capitalize()
-prezime = input('Unesite prezime studenta: ').capitalize()
-student['Podaci'] = ime + ' ' + prezime
-print('Student', student['Podaci'], 'je prijavio ispit iz kolegija',
-       student['Ime kolegija'], 'koji će se održati:', student['Datum ispita'])
+student['Ispit'] = ispit
+ime_studenta = input('Unesite ime studenta: ').capitalize()
+prezime_studenta = input('Unesite prezime studenta: ').capitalize()
+student['Podaci'] = ime_studenta + ' ' + prezime_studenta
+print('Student', student['Podaci'], '\nje prijavio ispit iz kolegija',
+       student['Ispit']['Kolegij']['Ime_kolegija'], '\nkoji će se održati:', student['Ispit']['Datum_ispita'])
