cfc04e54d40442ac5f63efee15568bf2f357068f
Daniel - Antonio Hedl (dhedl) <dhedl@tvz.hr>
2023-03-28 20:03:28 +0200

    3. laboratorijska vježba v2

--- LV3/lv3.py
+++ LV3/lv3.py
@@ -4,33 +4,40 @@ kolegiji = []
 
 broj_kolegija = int(input('Unesite broj kolegija: '))
 
-for i in range(broj_kolegija):
+for i in range(1, broj_kolegija+1):
     kolegij = {}
-    kolegij['ime'] = input('Unesite ime kolegija: ')
-    kolegij['ects'] = int(input(f"Unesite ECTS bodove za {i+1}. kolegij: "))
+    kolegij['ime'] = input(f"Unesite ime {i}. kolegija: ")
+    kolegij['ects'] = int(input(f"Unesite ECTS bodove za {i}. kolegij: "))
     kolegiji.append(kolegij)
 
-print('Popis svih kolegija: ')
-
-for kolegij in kolegiji:
-    print(f"\tKolegij {kolegij['ime']} nosi {kolegij['ects']} ECTS bodova ".expandtabs(8))
+# print('Popis svih kolegija: ')
+#
+# for kolegij in kolegiji:
+#     print(f"\tKolegij {kolegij['ime']} nosi {kolegij['ects']} ECTS bodova ".expandtabs(8))
 
 ispiti = []
 
 broj_ispita = int(input('Unesite broj ispita: '))
 
 print('Popis kolegija: ')
-for i, kolegij in enumerate(kolegiji):
-    print(f"{i+1}. {kolegij['ime']}")
 
-for i in range(broj_ispita):
-    odabir_kolegija = int(input('Unesite kolegij: '))
-    dan = int(input('Unesite dan ispita: '))
-    mjesec = int(input('Unesite mjesec ispita: '))
-    godina = int(input('Unesite godinu ispita: '))
-    datum = date(godina, mjesec, dan)
-    ispiti.append({'kolegij': kolegiji[odabir_kolegija-1], 'datum': datum})
+for i in range(1, broj_ispita+1):
+    ispit = {}
 
-print(ispiti)
+    for j, kolegij in enumerate(kolegiji, start=1):
+        print(f"\t{j}. {kolegij['ime']}")
 
-# TODO ispraviti, odabir kolegija, ispis unesenih ispita preko rječnika ispit
+    kolegij = int(input('Odaberite kolegij: '))
+
+    ispit['kolegij'] = kolegiji[kolegij-1]
+
+    dan = int(input(f'Unesite dan {i}. ispita: '))
+    mjesec = int(input(f'Unesite mjesec {i}. ispita: '))
+    godina = int(input(f'Unesite godinu {i}. ispita: '))
+
+    ispit['datum'] = date(godina, mjesec, dan)
+
+    ispiti.append(ispit)
+
+for ispit in ispiti:
+    print(f"Ispit iz kolegija {ispit['kolegij']['ime']} koji nosi {ispit['kolegij']['ects']} ECTS bodova održati će se {ispit['datum']}")
