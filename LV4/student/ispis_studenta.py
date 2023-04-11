4c53592ee82dc5e7fc78c8c94aa2ead3642bfebd
Daniel <dhedl@tvz.hr>
2023-04-11 14:33:15 +0200

    4. laboratorijska vježba v2

--- LV4/student/ispis_studenta.py
+++ LV4/student/ispis_studenta.py
@@ -1,2 +1,6 @@
-def ispis_studenta():
-    pass
+from ispit import ispis_ispita
+
+
+def ispis_studenta(student):
+    print(f"Student {student['ime']} {student['prezime']} je prijavio:")
+    ispis_ispita(student['ispit'])
