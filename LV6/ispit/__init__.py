daa59bd171bb5ef5d8f111f0d3b9629e9cdb6150
Daniel - Antonio Hedl (dhedl) <dhedl@tvz.hr>
2023-04-18 20:00:53 +0200

    6. laboratorijska vježba

---
+++ LV6/ispit/__init__.py
@@ -0,0 +1,2 @@
+from .ispis_ispita import ispis_ispita, get_ispit, ispis_svih_ispita
+from .unos_ispita import unos_ispita
---
+++ LV6/kolegij/__init__.py
@@ -0,0 +1,3 @@
+from .unos_kolegija import unos_kolegija
+from .ispis_kolegija import get_kolegij, ispis_svih_kolegija
+from .Kolegij import Kolegij
